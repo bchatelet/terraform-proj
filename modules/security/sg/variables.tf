8271fa535cf7685732630f8781a7af2ce3d672f3
bchatelet <bchatelet@wanadoo.fr>
2024-08-07 08:56:28 +0200

    Remove old-directory from the repository

--- modules/security/sg/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "The VPC ID where the security group will be created"
-  type        = string
-}
-
-variable "inbound_ports" {
-  type    = list(string)
-  default = []
-}
--- modules/storage/ebs/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ebs_volume_id" {
-  value = aws_ebs_volume.vol.*.id
-}
