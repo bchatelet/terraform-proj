ed94f08945c0dce87c970f03053863861eef0ef0
bchatelet <bchatelet@wanadoo.fr>
2024-08-13 19:59:00 +0200

    modif proj0, output added

--- proj0/modules/networking/eip/outputs.tf
+++ proj0/modules/networking/eip/outputs.tf
@@ -11,7 +11,7 @@ output "vpc_id" {
 output "vpc_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
-output "my_vpc_subnet_id" {
+output "my_vpc_subnet_id_output" {
   value = var.aws_instance_id
   }
 
