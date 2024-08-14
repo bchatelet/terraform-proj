5ada7a8aa336ef4c373a062a8e4cb9727695be75
bchatelet <bchatelet@wanadoo.fr>
2024-08-14 07:58:19 +0200

    Key pb

--- proj0/modules/databases/mongodb/outputs.tf
+++ proj0/modules/databases/mongodb/outputs.tf
@@ -19,3 +19,7 @@ output "mongodb_ami" {
 output "mongodb_key_name" {
   value = aws_instance.mongodb_instance.key_name
 }
+
+output "ec2_management_role_name" {
+  value = var.ec2_management_role_name
+}
